6339048cc77c347c5cbb10bd48f43ff54720da30
Henry-Cho <hyungseok5926@gmail.com>
2021-01-25 10:52:12 +0900

    JavaScript scrollintoView

--- main.js
+++ main.js
@@ -1,10 +1,50 @@
+'use strict'
 const toggle_Btn = document.querySelector('.navbar_toogle-Btn');
 const menu = document.querySelector('.nav_menu');
 const navbar_size = document.querySelector('.navbars');
+const navbar = document.querySelector('#navbars');
+const navbarHeight = navbar.getBoundingClientRect().height;
 // nav_bar toggle Btn activate
 toggle_Btn.addEventListener('click', ()=> {
     menu.classList.toggle('active');
-    console.log("menu active");
     navbar_size.classList.toggle('active');
-    console.log("navbar size");
 })
+
+// keyword 로 검색하기 Javascript scroll height
+// javascript element size
+document.addEventListener('scroll', ()=> {
+    //console.log(window.scrollY);
+    //console.log(`navbar height: ${navbarHeight}`);
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    }
+
+    else {
+        navbar.classList.remove('navbar--dark');
+    }
+})
+
+// scroll into view 만들기 (navbar)
+
+function scrollIntoView(event) {
+    const target = event.target.dataset;
+    const real_target = target.link;
+    if (real_target == null) {
+        return;
+    }
+    else {
+        document.getElementById(`${real_target}`).scrollIntoView({behavior: "smooth"});
+    }
+}
+
+const navbar_menu = document.querySelector('.nav_menu');
+
+navbar_menu.addEventListener('click', (event)=> {
+    scrollIntoView(event);
+})
+
+/* Scroll into View (Contact Me Button) */
+document.getElementById('home_contact_Btn').onclick = function() {
+    // Do whatever now that the user has clicked the link.
+    document.getElementById('contact').scrollIntoView({behavior: "smooth"});
+};
