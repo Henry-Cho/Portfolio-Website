522c777b27d9cd76bc850ad03faee63af888199b
Henry-Cho <hyungseok5926@gmail.com>
2021-02-03 10:56:20 +0900

    error handle
    data.json -> headers..

--- main.js
+++ main.js
@@ -3,7 +3,12 @@
 // upload items from json file by using fetch
 
 function uploadItems() {
-    return fetch('./data.json')
+    return fetch('./data.json', {
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+        })
     .then(response => response.json())
     .then(json => json.items);
 }
